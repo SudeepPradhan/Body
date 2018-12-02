86c453ac87160efbb425dcbebcbce12b47be4e65
Sudeep Pradhan <pradhan_sudeep@hotmail.com>
2018-12-02 11:54:23 -0700

    First

--- Brain/Nerve.cs
+++ Brain/Nerve.cs
@@ -2,7 +2,7 @@
 
 namespace Brain
 {
-    public class Class1
+    public class Nerve
     {
     }
 }
---
+++ Senses/Fetch.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Senses
+{
+    public class Fetch
+    {
+    }
+}
